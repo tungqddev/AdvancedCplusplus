35ad9f2db875694f675468c3b5356c94ca02867d
tungqddev <quachdaitung@gmail.com>
2017-10-02 07:45:57 +0700

    update 2017/10/02

---
+++ OverloadingTheDereferenceOperator/OverloadingTheDereferenceOperator.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "Complex.h"
+
+using namespace std;
+using namespace caveofprogramming;
+
+int main()
+{
+	Complex c1(2, 4);
+	std::cout << *c1 << endl;
+	cin.get();
+}
